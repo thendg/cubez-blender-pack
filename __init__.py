c01a118f6eb705695f8fcd0c988d353ca59a120b
lembn <lembn@outlook.com>
2022-06-02 13:21:56 +0100

    fixed bootloader

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-# The relative import implicitly registers the import classes too - equivalent to using bpy.utils.[register/unregister]_class
 from typing import Callable, Type
 
-from bpy.types import Context, Menu
+from bpy.types import Context, Menu, Operator
 
+# The relative import implicitly registers the import classes too - equivalent to using bpy.utils.[register/unregister]_class
 from .bqdm_exporter import BQDMExporter
 from .displacement_baker import DisplacementBaker
 from .utils import CubezOperator
@@ -13,7 +13,7 @@ bl_info = {
     "author": "NOIR Development Group",
     "version": (1, 0, 0),
     "blender": (3, 1, 0),
-    "warning": "This is an alpha version of the Cubez Blender Pack",
+    "warning": "This is an ALPHA version of the Cubez Blender Pack",
     "doc_url": "https://github.com/thendg/cubez-packages#readme",
     "tracker_url": "https://github.com/thendg/cubez-packages/issues",
     "support": "COMMUNITY",
@@ -21,27 +21,28 @@ bl_info = {
 classes: list[Type[CubezOperator]] = [BQDMExporter, DisplacementBaker]
 
 
-def get_menu_func(idname: str, label: str) -> Callable[[Menu, Context], None]:
+def get_menu_func(cls: Type[Operator]) -> Callable[[Menu, Context], None]:
     """
     Return a function that binds a button or trigger on a UILayout to the relevant class' `invoke()` method.
     Signature follows the requirements for this function to be passed as a draw function to a UI component.
 
-    :param idname: The bl_idname of the class to register.
-    :param label: The bl_label of the class to register.
+    :param cls: The class to register.
     """
-    return lambda caller, _context: caller.layout.operator(idname, text=label)
+    return lambda caller, _context: caller.layout.operator(
+        cls.bl_idname, text=cls.bl_label
+    )
 
 
 def register():
     "Append classes to their associated menus."
-    for cls in [cls for cls in classes if cls.get_menu_target()]:
-        cls.get_menu_target().append(get_menu_func(cls))
+    for cls in [cls for cls in classes if cls.menu_target]:
+        cls.menu_target.append(get_menu_func(cls))
 
 
 def unregister():
     "Remove classes from their associated menus."
-    for cls in [cls for cls in classes if cls.get_menu_target()]:
-        cls.get_menu_target().remove(get_menu_func(cls))
+    for cls in [cls for cls in classes if cls.menu_target]:
+        cls.menu_target.remove(get_menu_func(cls))
 
 
 if __name__ == "__main__":
--- dev/bootloader.py
+++ dev/bootloader.py
@@ -21,15 +21,18 @@ def bundle(
     :param output: The path to build the bundle to.
     """
 
+    # Delete old bundle
     if os.path.exists(output):
         if not overwrite:
             return
         os.remove(output)
 
+    # Get excluded files
     excluded = []
     for e in exclude:
         excluded += glob.glob(f"**/{e}", recursive=True)
 
+    # Create archive
     with ZipFile(output, "w") as archive:
         for ext in include:
             for file in glob.glob(f"**/*.{ext}", recursive=True):
@@ -38,6 +41,10 @@ def bundle(
 
 
 if __name__ == "__main__":
+    ################
+    # Bundle addon #
+    ################
+
     build_path = os.path.join(
         os.getcwd(),
         "build",
@@ -45,11 +52,24 @@ if __name__ == "__main__":
     )
 
     bundle(["py"], exclude=["dev/*"], output=build_path)
+
     if "--build" in sys.argv:
         print(f'Built to "{build_path}".')
     else:
+        ######################################
+        # Update and enable addon in Blender #
+        ######################################
+
         import bpy
 
-        bpy.ops.preferences.addon_remove(module=ARCHIVE_NAME)
+        # Calling addon_remove() attempts a GUI redraw, which breaks because it is being called
+        # from outside of an operator so we just catch the error that it raises
+        try:
+            # We have to use addon_remove() because calling addon_install(overwrite=True) doesn't
+            # delete old files from the addon, so we have to delete the whole addon and reinstall
+            bpy.ops.preferences.addon_remove(module=ARCHIVE_NAME)
+        except:
+            pass
         bpy.ops.preferences.addon_install(filepath=build_path)
         bpy.ops.preferences.addon_enable(module=ARCHIVE_NAME)
+        print("\nLOAD SUCCESSFUL")
